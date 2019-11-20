bdc370eaed43fb115ec7be5b14c58a1e1648e79f
ZKLu <597021238@qq.com>
2019-11-20 23:05:06 +0800

    2019/11/20 commit

--- app/src/main/java/com/samlu/togglebutton/MainActivity.java
+++ app/src/main/java/com/samlu/togglebutton/MainActivity.java
@@ -2,6 +2,7 @@ package com.samlu.togglebutton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.samlu.togglebutton.view.ToggleButton;
 
@@ -18,8 +19,16 @@ public class MainActivity extends AppCompatActivity {
 
     private void initUI() {
         toggleButton = (ToggleButton) findViewById(R.id.togglebutton);
-        toggleButton.setSlideBackgroundResource(R.drawable.slide_background);
+        toggleButton.setSlideBackgroundResource(R.drawable.slide_icon);
         toggleButton.setSwitchBackgroundResource(R.drawable.slide_background);
         toggleButton.setToggleState(ToggleButton.ToggleState.Open);
+
+        toggleButton.setOnToggleStateChangeListener(new ToggleButton.OnToggleStateChangeListener() {
+            @Override
+            public void onToggleStateChange(ToggleButton.ToggleState state) {
+                Toast.makeText(getApplicationContext(),state == ToggleButton.ToggleState.Open?"开启":"关闭",
+                        Toast.LENGTH_LONG);
+            }
+        });
     }
 }
--- app/src/main/java/com/samlu/togglebutton/view/ToggleButton.java
+++ app/src/main/java/com/samlu/togglebutton/view/ToggleButton.java
@@ -3,6 +3,9 @@ package com.samlu.togglebutton.view;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -12,15 +15,15 @@ import android.view.View;
 public class ToggleButton extends View{
 
     //开关状态
-    private ToggleState toggleState;
+    private ToggleState toggleState = ToggleState.Open;
     private Bitmap slideBg;
     private Bitmap switchBg;
+    private int currentX;//当前触摸点x轴坐标
+    private boolean isSlide ;
 
-    public ToggleButton(Context context) {
-        super(context);
+    public ToggleButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
     }
-
-
 
     public enum ToggleState{
         Open , Close
@@ -48,5 +51,93 @@ public class ToggleButton extends View{
     */
     public void setToggleState(ToggleState state) {
         toggleState = state;
+    }
+
+    /**设置当前控件显示在屏幕上的宽高
+    *@param
+    *@return
+    */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(switchBg.getWidth(),switchBg.getHeight());
+    }
+
+    /**绘制控件显示在屏幕上的样子
+    *@param
+    *@return
+    */
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        //绘制背景图片
+        //第二个参数left:要绘制的图片的左边缘的x轴坐标，第三个参数top:要绘制的图片的顶部的y轴左边
+        canvas.drawBitmap(switchBg,0,0,null);
+
+        //滑块的中心跟随鼠标
+        if (isSlide){
+            int left = currentX - slideBg.getWidth()/2;
+            if (left < 0) left =0;
+            if (left > switchBg.getWidth() - slideBg.getWidth()) left = switchBg.getWidth()-slideBg.getWidth();
+            canvas.drawBitmap(slideBg,left,0,null);
+        }else {
+            //停止滑动,根据state去绘制滑动块的位置
+            if (toggleState == ToggleState.Open){
+                //绘制滑动块的图片
+                canvas.drawBitmap(slideBg,switchBg.getWidth()-slideBg.getWidth(),0,null);
+            }else {
+                canvas.drawBitmap(slideBg,0,0,null);
+            }
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        currentX = (int) event.getX();
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                isSlide = true;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                break;
+            case MotionEvent.ACTION_UP:
+                isSlide = false;
+                int centerX = switchBg.getWidth()/2;
+                if (currentX > centerX){
+                    //open
+                    if (toggleState != ToggleState.Open){
+                        toggleState = ToggleState.Open;
+                        if (listener != null){
+                            listener.onToggleStateChange(toggleState);
+                        }
+                    }
+
+                }else {
+                    //close
+                    if (toggleState != ToggleState.Close){
+                        toggleState = ToggleState.Close;
+                        if (listener != null){
+                            listener.onToggleStateChange(toggleState);
+                        }
+                    }
+
+                }
+                if (listener != null){
+                    listener.onToggleStateChange(toggleState);
+                }
+                break;
+        }
+        invalidate();
+        return true;
+    }
+
+
+    private OnToggleStateChangeListener listener;
+    public  void setOnToggleStateChangeListener(OnToggleStateChangeListener listener){
+        this.listener = listener;
+    }
+
+    public interface  OnToggleStateChangeListener{
+        void onToggleStateChange(ToggleState state);
     }
 }
